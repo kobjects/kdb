e4bab22428e25138e13dd612828ec9dfaa6f0502
pleumann <pleumann>
2002-04-16 13:29:29 +0000

    Jetzt alles mit Interfaces

--- src/org/kobjects/db/DbRecord.java
+++ src/org/kobjects/db/DbRecord.java
@@ -35,178 +35,99 @@ import javax.microedition.rms.*;
  * Represents a record in a table. Actually, this class resembles an SQL result
  * set more than a record.
  */
-public abstract class DbRecord {
+public interface DbRecord {
 
-    protected DbTable table;
+    public void clear();
 
-    protected Object[] values;
+    public Object getObject(int column);
 
-    protected boolean modified;
+    public void setObject(int column, Object value);
 
-    protected boolean deleted;
+    public boolean getBoolean(int column);
 
-    protected DbRecord(DbTable table) {
-        this.table = table;
-    }
+    public int getInteger(int column);
 
-    protected Object[] getValues() {
-        return values;
-    }
+    public long getLong(int column);
 
-    protected void setValues(Object[] values) {
-        for (int i = 0; i < values.length; i++) {
-            this.values[i] = values[i];
-        }
-    }
+    public String getString(int column);
 
-    public void clear() {
-        values = new Object[getTable().getFieldCount()];
-        modified = true;
-    }
+    public byte[] getBinary(int column);
 
-    public Object getObject(int column) {
-        return values[column];
-    }
+    public void setBoolean(int column, boolean value);
 
-    public void setObject(int column, Object value) {
-        values[column] = value;
-        modified = true;
-    }
+    public void setInteger(int column, int value);
 
-    public boolean getBoolean(int column) {
-        Boolean b = (Boolean)getObject(column);
-        return (b == null) ? false : b.booleanValue();
-    }
+    public void setLong(int column, long value);
 
+    public void setString(int column, String value);
 
-    public int getInteger(int column) {
-        Integer i = (Integer)getObject(column);
-        return (i == null) ? 0 : i.intValue();
-    }
+    public void setBinary(int column, byte[] value);
 
+    public void refresh() throws DbException;
 
-    public long getLong(int column) {
-        Long l = (Long)getObject(column);
-        return (l == null) ? 0 : l.longValue();
-    }
+    public void update() throws DbException;
 
-    public String getString(int column) {
-        Object o = getObject(column);
-        return (o == null) ? null : o.toString();
-    }
+    public void insert() throws DbException;
 
-    public byte[] getBinary(int column) {
-        return (byte[])getObject(column);
-    }
-
-    public void setBoolean(int column, boolean value) {
-        setObject(column, new Boolean(value));
-    }
-
-    public void setInteger(int column, int value) {
-        setObject(column, new Integer(value));
-    }
-
-    public void setLong(int column, long value) {
-        setObject(column, new Long(value));
-    }
-
-    public void setString(int column, String value) {
-        setObject(column, value);
-    }
-
-    public void setBinary(int column, byte[] value) {
-        //byte[] bytes = new byte[value.length];
-        //System.arraycopy(value, 0, bytes, 0, value.length);
-        setObject(column, value); // was: bytes
-    }
-
-    public abstract void refresh() throws DbException;
-
-    public abstract void update() throws DbException;
-
-    public void insert() throws DbException {
-        modified = true;
-
-        DbTable table = getTable ();
-        int cnt = table.getFieldCount ();
-        for (int i = 0; i < cnt; i++) {
-            values[i] = table.getField(i).getDefault();
-        }
-    }
-
-    public void insert(Object[] values) throws DbException {
-        insert();
-
-        for (int i = 0; i < values.length; i++) {
-            setObject(i, values[i]);
-        }
-    }
+    public void insert(Object[] values) throws DbException;
 
     /**
      * Aktuellen Record löschen. Ändert nicht die Position innerhalb des
      * Result Sets. Stattdessen wird einfach nur "deleted" auf true gesetzt.
      */
-    abstract public void delete() throws DbException;
+    public void delete() throws DbException;
 
     /**
      * Alle Records in der Enumeration löschen.
      */
-    abstract public void deleteAll() throws DbException;
+    public void deleteAll() throws DbException;
 
-    public boolean isModified() {
-        return modified;
-    }
+    public boolean isModified();
 
-    public boolean isDeleted() {
-        return deleted;
-    }
+    public boolean isDeleted();
 
     /**
      * Returns the table this record belongs to.
      */
-    public abstract DbTable getTable();
+    public DbTable getTable();
 
     /**
      * Returns the ID of the current record.
      */
-    public abstract Object getId();
+    public Object getId();
 
     /**
      * Resets the iterator before the first row.
      */
-    public abstract void reset() throws DbException;
+    public void reset() throws DbException;
 
     /**
      * Queries whether the is a next element in this result set.
      */
-    public abstract boolean hasNextElement();
+    public boolean hasNextElement();
 
     /**
      * Proceeds to the next element in this result set, returning its record ID.
      */
-    public abstract Object nextElement() throws DbException;
+    public Object nextElement() throws DbException;
 
     /**
      * Returns the total number of rows.
      */
-    public abstract int getRowCount();
+    public int getRowCount();
 
     /**
      * Returns the current row number.
      */
-    public abstract int getRow();
+    public int getRow();
 
     /**
      * Jumps to a given row.
      */
-    public abstract void absolute(int row) throws DbException;
+    public void absolute(int row) throws DbException;
 
     /**
      * Throws away the record and all resources it has reserved.
      */
-    public void dispose() {
-        table = null;
-        values = null;
-    }
+    public void dispose();
 }
--- src/org/kobjects/db/DbTable.java
+++ src/org/kobjects/db/DbTable.java
@@ -16,170 +16,86 @@ import javax.microedition.rms.*;
 /**
  * Represents a database table.
  */
-public abstract class DbTable {
-
-    /**
-     * Holds the fields that are defined for this table.
-     */
-    protected Vector fields = new Vector();
-
-    /**
-     * Connects to a table. This factory method examines the given connector and
-     * loads a suitable table implementation by name, connecting it to the given
-     * table. The result is the same as for instatiating and connecting the
-     * table explicitly, i.e. the table is not opened. The connector string
-     * obeys the following URI-like naming scheme:
-     * <pre>
-     *   "protocol:table;parameters"
-     * </pre>
-     * The actual table implementation is chosen depending on the "protocol"
-     * value. The package name always is "org.kobjects.db" plus a sub-package
-     * named after the protocol. The class name consists of the protocol values
-     * with the first letter being uppercased plus the fixed suffix "Table". As
-     * an example, for a connector string
-     * <pre>
-     *   "rms:MyTable;user=joerg;password=secret"
-     * </pre>
-     * the class <code>org.kobjects.db.rms.RmsTable</code> would be instantiated
-     * and the new instance connects to a table "MyTable". Interpretation of the
-     * parameters following the ";" is up to the table implementation.
-     */
-    public static DbTable connect(String connector) throws DbException {
-        DbTable table = null;
-
-        try {
-            int p = connector.indexOf(':');
-            String type = connector.substring(0, p);
-            if ("https".equals(type)) type = "http";
-            String name = Character.toUpperCase(type.charAt(0)) + type.substring(1);
-
-            table = (DbTable)Class.forName("org.kobjects.db." + type + "." + name + "Table").newInstance();
-        }
-        catch (Exception e) {
-            throw new DbException("Can't connect to table \"" + connector + "\"", e);
-        }
-
-        table.init(connector);
-
-        return table;
-    }
-
-    /**
-     * Checks the table's state. The method throws an exception if the table's
-     * actual open/closed state doesn't match the required state.
-     */
-    protected void checkState(boolean open) throws DbException {
-        if (isOpen() != open) {
-            throw new DbException("Table \"" + getName() + "\" not " + (open ? "open" : "closed"));
-        }
-    }
+public interface DbTable {
 
     /**
      * Is called to connect to a table. Note that the given connector follows
      * the syntax used in the connect() factory method, so the actual table
      * name has to be derived from it.
      */
-    protected abstract void init(String connector);
+    public void connect(String connector);
 
     /**
      * Returns the table's name.
      */
-    public abstract String getName();
+    public String getName();
+
     /**
      * Returns true if the table exists and false otherwise.
      */
-    public abstract boolean exists();
+    public boolean exists();
 
     /**
      * Returns true if the table is open and false otherwise.
      */
-    public abstract boolean isOpen();
+    public boolean isOpen();
 
     /**
      * Deletes the table. Succeeds if the table doesn't exist. Throws an error
      * if the table cannot be deleted for some reason.
      */
-    public abstract void delete() throws DbException;
+    public void delete() throws DbException;
 
     /**
      * Creates the table. This method creates a new physical table or overwrites
      * an existing one. The method can only be called when the table is closed.
      * The new table will not automatically be opened.
      */
-    public abstract void create() throws DbException;
+    public void create() throws DbException;
 
     /**
      * Opens the table. Must throw an exception if the table is already open or
      * some other error occurs.
      */
-    public abstract void open() throws DbException;
+    public void open() throws DbException;
 
     /**
      * Closes the table. Always succeeds.
      */
-    public abstract void close();
+    public void close();
 
     /**
      * Adds a field to the table. The table must be closed for this method
      * to succeed.
      */
-    public DbField addField(String name, int type) throws DbException {
-        checkState(false);
-
-        if (findField(name) != -1) {
-            throw new DbException("Field \"" + name + "\" already exists in table \"" + getName() + "\"");
-        }
-
-        if (fields.size() == 255) {
-            throw new DbException("Field IDs in table \"" + getName() + "\" exhausted.");
-        }
-
-        DbField field = new DbField(this, fields.size(), name, type);
-        fields.addElement(field);
-
-        return field;
-    }
+    public DbField addField(String name, int type) throws DbException;
 
     /**
      * Returns the total number of fields.
      */
-    public int getFieldCount() {
-        return fields.size();
-    }
+    public int getFieldCount();
 
     /**
      * Returns the field with the given index.
      */
-    public DbField getField(int i) {
-        return (DbField)fields.elementAt(i);
-    }
+    public DbField getField(int i);
 
     /**
      * Returns the index of the given field or -1 if the field does not exist.
      */
-    public int findField(String name) {
-        for (int i = 0; i < fields.size(); i++) {
-            if (getField(i).getName().equals(name)) return i;
-        }
-
-        return -1;
-    }
+    public int findField(String name);
 
     /**
      * Select all records. This is a convenience method. The usual rules for
      * the "big" select() apply.
      */
-    public DbRecord select(boolean updated) throws DbException {
-         return select(null, -1, false, updated);
-    }
+    public DbRecord select(boolean updated) throws DbException;
 
     /**
      * Selects the record that bears the given ID. This is a convenience
      * method. The usual rules for the "big" select() apply.
      */
-    public DbRecord select(Object id) throws DbException {
-        return select(new DbCondition(DbCondition.EQ, -1, id), -1, false, false);
-    }
+    public DbRecord select(Object id) throws DbException;
 
     /**
      * Select all records for which the given condition evaluates to
@@ -190,5 +106,5 @@ public abstract class DbTable {
      * whole result set is kept updated or not. The returned result set is
      * positioned before the first record.
      */
-    public abstract DbRecord select(DbCondition filter, int oderBy, boolean reverse, boolean updated) throws DbException;
+    public DbRecord select(DbCondition filter, int oderBy, boolean reverse, boolean updated) throws DbException;
 }

a9a0f7012838434a0dd76c683e72ce646cad041f
haustein <haustein>
2002-07-29 21:03:47 +0000

    ls8bib empty starting point added

---
+++ src_j2se/org/kobjects/db/ls8bib/Ls8bibTable.java
@@ -0,0 +1,13 @@
+package org.kobjects.db.ls8bib;
+
+import org.kobjects.db.bibtex.BibtexTable;
+
+/**
+ * @author Stefan Haustein
+ *
+ * To change this generated comment edit the template variable "typecomment":
+ * Window>Preferences>Java>Templates.
+ */
+public class Ls8bibTable extends BibtexTable {
+
+}

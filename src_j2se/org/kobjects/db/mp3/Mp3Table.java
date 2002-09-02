47b59966da55faaed45944ed4c9d84480de4e560
haustein <haustein>
2002-09-02 19:41:38 +0000

    min. size check added

--- src_j2se/org/kobjects/db/mp3/Mp3Table.java
+++ src_j2se/org/kobjects/db/mp3/Mp3Table.java
@@ -16,102 +16,104 @@ import java.util.*;
  */
 public class Mp3Table extends RamTable {
 
-	String base;
-	static int counter;
-	
-	/**
-	 * @see org.kobjects.db.DbTable#connect(String)
-	 */
-	public void connect(String connector) throws DbException {
+    String base;
+    static int counter;
 
-		base = connector.substring (4);	
-		exists = true;
+    /**
+     * @see org.kobjects.db.DbTable#connect(String)
+     */
+    public void connect(String connector) throws DbException {
 
-		addField ("id", DbField.STRING);      // 0
-		addField ("file", DbField.STRING);    // 1 
-		addField ("track", DbField.STRING);   // 2
-		addField ("artist", DbField.STRING);  // 3
-		addField ("album", DbField.STRING);	  // 4
-		addField ("year", DbField.STRING);    // 5
-		addField ("comment", DbField.STRING); // 6 
-		addField ("data", DbField.BINARY);    // 7
+        base = connector.substring(4);
+        exists = true;
 
-	}
+        addField("id", DbField.STRING); // 0
+        addField("file", DbField.STRING); // 1 
+        addField("track", DbField.STRING); // 2
+        addField("artist", DbField.STRING); // 3
+        addField("album", DbField.STRING); // 4
+        addField("year", DbField.STRING); // 5
+        addField("comment", DbField.STRING); // 6 
+        addField("data", DbField.BINARY); // 7
 
-	/**
-	 * @see org.kobjects.db.DbTable#getName()
-	 */
-	public String getName() {
-		return "mp3";
-	}
+    }
 
-	String clean (String s) {
-		if (s == null) return null;
-		int cut = s.indexOf ('\0');
-		if (cut != -1) s = s.substring (0, cut);
-		return s.trim ();
-	}
+    /**
+     * @see org.kobjects.db.DbTable#getName()
+     */
+    public String getName() {
+        return "mp3";
+    }
 
-	void addFile (File file) throws DbException {
-		Object[] data = new Object [getFieldCount ()];
-		data [0] = ""+(counter++);
-		data [1] = file.toString ();
-		records.addElement (data);	
-		
-		try {
-			RandomAccessFile raf = new RandomAccessFile (file, "r");
-			raf.seek(file.length()-128);
-			byte [] buf = new byte [128];
-			raf.readFully (buf);
-			raf.close ();
-			if (new String (buf, 0, 3).equals("TAG")) {
-				data[2] = clean(new String(buf, 3, 30));  // track
-				data[3] = clean(new String(buf, 33, 30)); //artist
-				data[4] = clean(new String(buf, 63, 30)); // album
-				data[5] = clean(new String(buf, 93, 4)); //year
-				data[6] = clean(new String(buf, 97, 127-97)); // comment
-			}	
-		}
-		catch (IOException e) {
-			throw new DbException (e.toString());
-		}
-	}
+    String clean(String s) {
+        if (s == null)
+            return null;
+        int cut = s.indexOf('\0');
+        if (cut != -1)
+            s = s.substring(0, cut);
+        return s.trim();
+    }
 
-	void recurse (File dir) throws DbException {
-		File [] files = dir.listFiles ();	
-		
-		for (int i = 0; i < files.length; i++) {
-			File file = files [i];
-			if (file.isDirectory ()) recurse (file);
-			else if (file.getName ().toLowerCase ().endsWith (".mp3")) {
-				addFile (file);	
-			}
-		}
-	}
+    void addFile(File file) throws DbException {
+        Object[] data = new Object[getFieldCount()];
+        data[0] = "" + (counter++);
+        data[1] = file.toString();
+        records.addElement(data);
 
+        if (file.length() > 128) {
+            try {
+                RandomAccessFile raf = new RandomAccessFile(file, "r");
+                raf.seek(file.length() - 128);
+                byte[] buf = new byte[128];
+                raf.readFully(buf);
+                raf.close();
+                if (new String(buf, 0, 3).equals("TAG")) {
+                    data[2] = clean(new String(buf, 3, 30)); // track
+                    data[3] = clean(new String(buf, 33, 30)); //artist
+                    data[4] = clean(new String(buf, 63, 30)); // album
+                    data[5] = clean(new String(buf, 93, 4)); //year
+                    data[6] = clean(new String(buf, 97, 127 - 97)); // comment
+                }
+            }
+            catch (IOException e) {
+                throw new DbException(e.toString());
+            }
+        }
+    }
 
-	public void open() throws DbException {
-		super.open();
-		recurse (new File (base)); 		
-	}
-	
-	
-	public static void main (String [] argv)  throws DbException {
-	
-		DbTable table = DbManager.connect("mp3:/Users/haustein/Music");
-		table.open ();
-		DbRecord record = table.select (false);	
-		
-		while (record.hasNext ()) {
-			record.next();
-			for (int i = 0; i < table.getFieldCount ()-1; i++) {
-				System.out.println (record.getString (i));
-			}
-		}
-	}
-	
-	
-	protected RamRecord getRecords (Vector selected, int[] fields) {
-		return new Mp3Record (this, selected, fields);
-	}
+    void recurse(File dir) throws DbException {
+        File[] files = dir.listFiles();
+
+        for (int i = 0; i < files.length; i++) {
+            File file = files[i];
+            if (file.isDirectory())
+                recurse(file);
+            else if (file.getName().toLowerCase().endsWith(".mp3")) {
+                addFile(file);
+            }
+        }
+    }
+
+    public void open() throws DbException {
+        super.open();
+        recurse(new File(base));
+    }
+
+    public static void main(String[] argv) throws DbException {
+
+        DbTable table = DbManager.connect("mp3:/Users/haustein/Music");
+        table.open();
+        DbRecord record = table.select(false);
+
+        while (record.hasNext()) {
+            record.next();
+            for (int i = 0; i < table.getFieldCount() - 1; i++) {
+                System.out.println(record.getString(i));
+            }
+        }
+    }
+
+    protected RamRecord getRecords(Vector selected, int[] fields) {
+        return new Mp3Record(this, selected, fields);
+    }
 }
